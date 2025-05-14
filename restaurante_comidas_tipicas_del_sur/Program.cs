3a174620308d5273791cd59cc2098d3b74478d18
Samuel <scardona0297@gmail.com>
2025-05-14 10:56:02 -0500

    Se actualiza la configraucion de la BD apuntando a una BD en linea, en sommee

--- restaurante_comidas_tipicas_del_sur/Program.cs
+++ restaurante_comidas_tipicas_del_sur/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using restaurante_comidas_tipicas_del_sur.Entity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RestauranteComidaTipicaDelSurContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
